8f157783a7d92c0e68d9ea9a8c3e158d5bd20b3a
johnerikhalse <johnerikhalse@gmail.com>
2019-07-02 13:12:24 +0200

    CI fixes

--- .travis/push_script.sh
+++ .travis/push_script.sh
@@ -3,11 +3,9 @@
 DOCKER_TAG=latest
 
 if [[ -n "$TRAVIS_TAG" || "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
-  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-
   if [[ -n "${TRAVIS_TAG}" ]]; then
     DOCKER_TAG=${TRAVIS_TAG}
   fi
 
-  mvn -B -Pdocker-build-and-push -Ddocker.tag="$TRAVIS_TAG" install;
+  mvn -B -Pdocker-build-and-push -Ddocker.tag="$DOCKER_TAG" install;
 fi
